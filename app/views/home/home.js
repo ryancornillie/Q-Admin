ee0050beddf7de40b19d02a911725218e4efcbc4
Ryan Cornillie <cornil15@msu.edu>
2017-01-21 04:03:12 -0500

    side nav

--- app/views/home/home.js
+++ app/views/home/home.js
@@ -2,10 +2,11 @@
 
 angular.module('myApp.home', [])
 
-  .controller('HomeCtrl', function($scope) {
-
-    $scope.offices = [ {location: "3467 EB", id:1, course: "ME 340"}, {location: "432 Wells", id:2, course: "MTH 223"} ];
+  .controller('HomeCtrl', function($scope, $mdSidenav) {
 
     $scope.queue = [ {name: "Timmy Daniel"}, {name: "Irene Baldwin"}, {name: "Robin	Marsh"}, {name: "Hubert	Cortez"} ];
 
-});
+
+
+
+  });
--- app/views/nav/nav.js
+++ app/views/nav/nav.js
@@ -6,13 +6,23 @@
 
 angular.module('myApp.nav', [])
 
-    .controller('NavCtrl', function($scope, $state) {
+    .controller('NavCtrl', function($scope, $state, $mdSidenav) {
 
+        $scope.offices = [ {location: "3467 EB", id:1, course: "ME 340"}, {location: "432 Wells", id:2, course: "MTH 223"} ];
 
         $scope.logout = function () {
 
             $state.go("login");
         };
+
+        $scope.toggleLeft = buildToggler('left');
+        $scope.toggleRight = buildToggler('right');
+
+        function buildToggler(componentId) {
+            return function() {
+                $mdSidenav(componentId).toggle();
+            }
+        }
 
 
     });
